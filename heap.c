5f5339aa0a67f4973d59464f99d2365908c91db6
MarbiFox <>
2022-05-05 01:10:34 +0000

    new try

--- heap.c
+++ heap.c
@@ -17,6 +17,21 @@ typedef struct Heap{
 } Heap;
 
 
+void swap(heapElem * parent, heapElem * node) {
+   heapElem * aux = (heapElem *) malloc (sizeof(heapElem));
+   aux = node;
+   node = parent;
+   parent = aux;
+}
+
+Heap* createHeap(){
+   Heap * new = (Heap *) malloc (sizeof(Heap));
+   new->heapArray = (heapElem *) malloc (10 * sizeof(heapElem));
+   new->size = 0;
+   new->capac = 10;
+   return new;
+}
+
 void* heap_top(Heap* pq){
     return NULL;
 }
@@ -24,7 +39,23 @@ void* heap_top(Heap* pq){
 
 
 void heap_push(Heap* pq, void* data, int priority){
+   //Insertar al final del árbol/arreglo.
+   if (pq->heapArray[pq->size].priority == NULL){
+      //enlarge(pq->heapArray);
+   }
+   pq->heapArray[pq->size].priority = priority;
+   pq->heapArray[pq->size].data = data;
 
+   //Reordenar si es necesario.
+   int aux = pq->size;
+   int parent = (aux-1)/2;
+   while (parent >= 0){
+      //Comparar prioridad del nodo con el de su "Padre".
+      if (pq->heapArray[parent].priority > pq->heapArray[aux].priority) break;
+      swap(&(pq->heapArray[parent]), &(pq->heapArray[aux]));
+      parent = (parent-1)-2;
+   }
+   pq->size++;
 }
 
 
@@ -32,7 +63,4 @@ void heap_pop(Heap* pq){
 
 }
 
-Heap* createHeap(){
 
-   return NULL;
-}
