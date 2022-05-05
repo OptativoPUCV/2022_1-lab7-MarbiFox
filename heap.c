b4933674302fc50e371b22f4b650cf4273a29156
MarbiFox <>
2022-05-05 01:59:35 +0000

    new try

--- heap.c
+++ heap.c
@@ -60,8 +60,9 @@ void heap_push(Heap* pq, void* data, int priority){
    //Reordenar si es necesario.
    int aux = i;
    int parent = (aux-1)/2;
-   while (parent >= 0 && pq->heapArray[parent].priority > pq->heapArray[aux].priority){
+   while (parent >= 0){
       //Comparar prioridad del nodo con el de su "Padre".
+      if (pq->heapArray[parent].priority > pq->heapArray[aux].priority) break;
       swap(&(pq->heapArray[parent]), &(pq->heapArray[aux]));
       parent = (parent-1)/2;
    }
